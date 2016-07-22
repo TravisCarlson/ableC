48ef042d4c53cc722424af134ca7ecad07161fef
Travis Carlson <carl4980@umn.edu>
2016-07-22 11:00:53 -0500

    sqlite: some progress on step loop, new syntax for open/close

--- extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/abstractsyntax/AbstractSyntax.sv
+++ extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/abstractsyntax/AbstractSyntax.sv
@@ -4,14 +4,100 @@ imports edu:umn:cs:melt:ableC:abstractsyntax as abs;
 imports edu:umn:cs:melt:ableC:abstractsyntax:construction as abs;
 imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
 
-abstract production sqliteQueryExpr
+abstract production sqliteUse
+top::abs:Expr ::= dbname::String
+{
+  {-- want to forward to:
+    sqlite3 *_db;
+    sqlite3_open(${dbname}, &_db);
+  -}
+
+  local db :: abs:Name = abs:name("_db", location=top.location);
+
+  -- sqlite3 *_db;
+  local dbDecl :: abs:Stmt =
+    abs:declStmt(
+      abs:variableDecls(
+        [],
+        [],
+        abs:typedefTypeExpr(
+          [],
+          abs:name("sqlite3", location=top.location)
+        ),
+        abs:foldDeclarator([
+          abs:declarator(
+            db,
+            abs:pointerTypeExpr(
+              [],
+              abs:baseTypeExpr()
+            ),
+            [],
+            abs:nothingInitializer()
+          )
+        ])
+      )
+    );
+
+  -- sqlite3_open(${dbname}, &_db);
+  local callOpen :: abs:Expr =
+    abs:directCallExpr(
+      abs:name("sqlite3_open", location=top.location),
+      abs:foldExpr([
+        abs:stringLiteral(dbname, location=top.location),
+        abs:unaryOpExpr(
+          abs:addressOfOp(location=top.location),
+          abs:declRefExpr(db, location=top.location),
+          location=top.location
+        )
+      ]),
+      location=top.location
+    );
+
+  local fullExpr :: abs:Expr =
+    abs:stmtExpr(
+      abs:foldStmt([
+        dbDecl,
+        abs:exprStmt(callOpen)
+      ]),
+
+      abs:declRefExpr(
+        db,
+        location=top.location
+      ),
+      location=top.location
+    );
+
+  forwards to fullExpr;
+}
+
+abstract production sqliteExit
+top::abs:Expr ::= db::abs:Name
+{
+  {-- want to forward to:
+    sqlite3_close(${db});
+  -}
+
+  -- sqlite3_close(${db});
+  local callClose :: abs:Expr =
+    abs:directCallExpr(
+      abs:name("sqlite3_close", location=top.location),
+      abs:foldExpr([
+        abs:declRefExpr(db, location=top.location)
+      ]),
+      location=top.location
+    );
+
+    forwards to callClose;
+}
+
+abstract production sqliteQuery
 top::abs:Expr ::= db::abs:Name query::String
 {
-{-- want to forward to this
-  const char *_query = ${query};
-  sqlite3_stmt *_stmt;
-  sqlite3_prepare(${db}, _query, sizeof(query), &_stmt, NULL);
--}
+  {-- want to forward to:
+    const char *_query = ${query};
+    sqlite3_stmt *_stmt;
+    sqlite3_prepare(${db}, _query, sizeof(query), &_stmt, NULL);
+  -}
 
   local stmt :: abs:Name = abs:name("_stmt", location=top.location);
 
@@ -68,7 +154,6 @@ top::abs:Expr ::= db::abs:Name query::String
       location=top.location
     );
 
-
   local fullExpr :: abs:Expr =
     abs:stmtExpr(
       abs:foldStmt([
@@ -82,7 +167,63 @@ top::abs:Expr ::= db::abs:Name query::String
       ),
       location=top.location
     );
+
   forwards to fullExpr;
+}
+
+abstract production sqliteForeach
+top::abs:Stmt ::= row::abs:Name stmt::abs:Name body::abs:Stmt
+{
+  {-- want to forward to:
+    struct _row_t {
+      const unsigned char *one;
+      int two;
+    };
+    sqlite3_reset(${stmt});
+    while (sqlite3_step(${stmt}) == SQLITE_ROW) {
+      struct _row_t ${row};
+      ${row}.one = sqlite3_column_text(${stmt}, 0);
+      ${row}.two = sqlite3_column_int(${stmt}, 1);
+      ${body};
+    }
+  -}
+
+  -- SQLITE_ROW
+  local sqlite_row :: abs:Expr =
+    -- TODO: don't hardcode value
+    abs:mkIntConst(100, builtIn());
+--    abs:declRefExpr(
+--      abs:name("SQLITE_ROW", location=builtIn()),
+--      location=builtIn()
+--    );
+
+  -- sqlite3_step(${stmt})
+  local callStep :: abs:Expr =
+    abs:directCallExpr(
+      abs:name("sqlite3_step", location=builtIn()),
+      abs:foldExpr([
+        abs:declRefExpr(stmt, location=builtIn())
+      ]),
+      location=builtIn()
+    );
+
+  -- sqlite3_step(${stmt}) == SQLITE_ROW
+  local hasRow :: abs:Expr =
+    abs:binaryOpExpr(
+      callStep,
+      abs:compareOp(abs:equalsOp(location=builtIn()), location=builtIn()),
+      sqlite_row,
+      location=builtIn()
+    );
+
+  -- while (sqlite3_step(${stmt}) == SQLITE_ROW) { ${body}; }
+  local whileHasRow :: abs:Stmt =
+    abs:whileStmt(
+      hasRow,
+      body
+    );
+
+  forwards to whileHasRow;
 }
 
 -- TODO: can this be used from ableC:abstractsyntax instead of copied?
@@ -91,4 +232,12 @@ abs:Name ::= n::cnc:Identifier_t
 {
   return abs:name(n.lexeme, location=n.location);
 }
+
+-- New location for expressions which don't have real locations
+abstract production builtIn
+top::Location ::=
+{
+  forwards to loc("Built In", 0, 0, 0, 0, 0, 0);
+}
+
 
--- extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/concretesyntax/ConcreteSyntax.sv
+++ extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/concretesyntax/ConcreteSyntax.sv
@@ -4,14 +4,29 @@ imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
 imports edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax as abs;
 import silver:langutil;
 
-marking terminal SqliteOn_t 'on' lexer classes {Ckeyword};
+marking terminal Sqlite_t 'sqlite' lexer classes {Ckeyword};
 
 terminal SqliteQuery_t 'query';
+terminal SqliteUse_t 'use';
+terminal SqliteExit_t 'exit';
+terminal SqliteFor_t 'for';
+
+concrete production sqliteUse_c
+top::cnc:PrimaryExpr_c ::= Sqlite_t SqliteUse_t dbname::cnc:StringConstant_c
+{
+  top.ast = abs:sqliteUse(dbname.ast, location=top.location);
+}
+
+concrete production sqliteExit_c
+top::cnc:PrimaryExpr_c ::= Sqlite_t SqliteExit_t db::cnc:Identifier_t
+{
+  top.ast = abs:sqliteExit(abs:fromId(db), location=top.location);
+}
 
 concrete production sqliteQueryDb_c
-top::cnc:PrimaryExpr_c ::= SqliteOn_t db::cnc:Identifier_t SqliteQuery_t '{' query::SqliteQuery_c '}'
+top::cnc:PrimaryExpr_c ::= Sqlite_t SqliteQuery_t db::cnc:Identifier_t '{' query::SqliteQuery_c '}'
 {
-  top.ast = abs:sqliteQueryExpr(abs:fromId(db), query.ast, location=top.location);
+  top.ast = abs:sqliteQuery(abs:fromId(db), query.ast, location=top.location);
 }
 
 nonterminal SqliteQuery_c with location, ast<String>;
@@ -21,4 +36,11 @@ concrete productions top::SqliteQuery_c
   {
     top.ast = s.ast;
   }
+
+concrete production sqliteForeach_c
+top::cnc:Stmt_c ::= Sqlite_t SqliteFor_t '(' row::cnc:Identifier_t ':'
+                           stmt::cnc:Identifier_t ')' '{' body::cnc:Stmt_c '}'
+{
+  top.ast = abs:sqliteForeach(abs:fromId(row), abs:fromId(stmt), body.ast);
+}
 
