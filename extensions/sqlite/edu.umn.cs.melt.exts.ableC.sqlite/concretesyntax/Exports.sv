bdabd5ef6d6c67a48195678802f074964f7304c2
Travis Carlson <carl4980@umn.edu>
2016-07-25 15:22:00 -0500

    sqlite: move query syntax from On.sv to Query.sv

---
+++ extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/concretesyntax/Exports.sv
@@ -0,0 +1,5 @@
+grammar edu:umn:cs:melt:exts:ableC:sqlite:concretesyntax;
+
+exports edu:umn:cs:melt:exts:ableC:sqlite:concretesyntax:use;
+exports edu:umn:cs:melt:exts:ableC:sqlite:concretesyntax:sqliteOn;
+
