27b8f9dc021ab51507a0b7f3da49a05b49ec3032
Travis Carlson <carl4980@umn.edu>
2016-08-04 14:55:53 -0500

    sqlite: split foreach into separate grammar

--- extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/abstractsyntax/Exports.sv
+++ extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/abstractsyntax/Exports.sv
@@ -1,5 +1,6 @@
 grammar edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax;
 
+exports edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax:foreach;
 exports edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax:sqliteOn;
 exports edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax:tables;
 exports edu:umn:cs:melt:exts:ableC:sqlite:abstractsyntax:types;
--- extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/concretesyntax/sqliteOn/On.sv
+++ extensions/sqlite/edu.umn.cs.melt.exts.ableC.sqlite/concretesyntax/sqliteOn/On.sv
@@ -6,7 +6,6 @@ import silver:langutil;
 import edu:umn:cs:melt:exts:ableC:sqlite:concretesyntax:sqliteOn:query;
 
 marking terminal SqliteOn_t 'on' lexer classes {Ckeyword};
-marking terminal SqliteForeach_t 'foreach' lexer classes {Ckeyword};
 
 terminal SqliteExit_t 'exit';
 terminal SqliteQuery_t 'query';
@@ -16,13 +15,6 @@ concrete production sqliteExit_c
 top::cnc:PrimaryExpr_c ::= 'on' db::cnc:Expr_c 'exit'
 {
   top.ast = abs:sqliteExit(db.ast, location=top.location);
-}
-
-concrete production sqliteForeach_c
-top::cnc:Stmt_c ::= 'foreach' '(' row::cnc:Identifier_t ':'
-                           stmt::cnc:Expr_c ')' body::cnc:Stmt_c
-{
-  top.ast = abs:sqliteForeach(abs:fromId(row), stmt.ast, body.ast);
 }
 
 concrete production sqliteQueryDb_c
