50a94f27e5c8229cd53567ece3073084337c4f8b
Travis Carlson <carl4980@umn.edu>
2016-08-08 11:15:59 -0500

    yacc: implement more of the abstract syntax tree

--- extensions/yacc/edu.umn.cs.melt.exts.ableC.yacc/abstractsyntax/AbstractSyntax.sv
+++ extensions/yacc/edu.umn.cs.melt.exts.ableC.yacc/abstractsyntax/AbstractSyntax.sv
@@ -4,46 +4,115 @@ imports edu:umn:cs:melt:ableC:abstractsyntax as abs;
 imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
 
 abstract production yaccGrammar
-top::abs:Decl ::= YaccProductionList
+top::abs:Stmt ::= YaccProductionList
 {
-  forwards to abs:decls(abs:nilDecl());
+  forwards to abs:nullStmt();
 }
 
 nonterminal YaccDeclList with decls;
 synthesized attribute decls :: [YaccDecl];
-
-abstract production yaccDeclList
+abstract production yaccConsDecl
 top::YaccDeclList ::= p::YaccDecl ps::YaccDeclList
 {
   top.decls = cons(p, ps.decls);
 }
-
-abstract production yaccNilDeclList
+abstract production yaccNilDecl
 top::YaccDeclList ::=
 {
   top.decls = nil();
 }
 
 nonterminal YaccDecl;
-
-abstract production yaccDecl
-top::YaccDecl ::=
+abstract production yaccTokenDecl
+top::YaccDecl ::= n::abs:Name
+{
+}
+abstract production yaccStartDecl
+top::YaccDecl ::= n::abs:Name
+{
+}
+abstract production yaccLeftDecl
+top::YaccDecl ::= ns::YaccNameList
 {
 }
 
 nonterminal YaccNameList with names;
 synthesized attribute names :: [abs:Name];
-
-abstract production yaccNameList
+abstract production yaccConsName
 top::YaccNameList ::= n::abs:Name ns::YaccNameList
 {
   top.names = cons(n, ns.names);
 }
-
-abstract production yaccNilNameList
+abstract production yaccNilName
 top::YaccNameList ::=
 {
   top.names = nil();
+}
+
+nonterminal YaccProductionList with prods;
+synthesized attribute prods :: [YaccProduction];
+abstract production yaccConsProduction
+top::YaccProductionList ::= p::YaccProduction ps::YaccProductionList
+{
+  top.prods = cons(p, ps.prods);
+}
+abstract production yaccNilProduction
+top::YaccProductionList ::=
+{
+  top.prods = nil();
+}
+
+nonterminal YaccProduction with leftNt, prodAlts;
+synthesized attribute leftNt :: abs:Name;
+synthesized attribute prodAlts :: [YaccProductionAlternative];
+abstract production yaccProduction
+top::YaccProduction ::= leftNt::abs:Name pas::YaccProductionAlternativeList
+{
+  top.leftNt = leftNt;
+  top.prodAlts = pas.prodAlts;
+}
+
+nonterminal YaccProductionAlternativeList with prodAlts;
+abstract production yaccConsProductionAlternative
+top::YaccProductionAlternativeList ::= pa::YaccProductionAlternative pas::YaccProductionAlternativeList
+{
+  top.prodAlts = cons(pa, pas.prodAlts);
+}
+
+abstract production yaccNilProductionAlternative
+top::YaccProductionAlternativeList ::=
+{
+  top.prodAlts = nil();
+}
+
+nonterminal YaccProductionAlternative with symOrActs;
+synthesized attribute symOrActs :: [YaccSymbolOrAction];
+abstract production yaccProductionAlternative
+top::YaccProductionAlternative ::= s::YaccSymbolOrActionList
+{
+  top.symOrActs = s.symOrActs;
+}
+
+nonterminal YaccSymbolOrActionList with symOrActs;
+abstract production yaccConsSymbolOrAction
+top::YaccSymbolOrActionList ::= sa::YaccSymbolOrAction sas::YaccSymbolOrActionList
+{
+  top.symOrActs = cons(sa, sas.symOrActs);
+}
+abstract production yaccNilSymbolOrAction
+top::YaccSymbolOrActionList ::=
+{
+  top.symOrActs = nil();
+}
+
+nonterminal YaccSymbolOrAction;
+abstract production yaccSymbol
+top::YaccSymbolOrAction ::= n::abs:Name
+{
+}
+abstract production yaccSemanticAction
+top::YaccSymbolOrAction ::= s::abs:Stmt
+{
 }
 
 function fromId
@@ -52,94 +121,3 @@ abs:Name ::= n::cnc:Identifier_t
   return abs:name(n.lexeme, location=n.location);
 }
 
-nonterminal YaccProductionList with prods;
-synthesized attribute prods :: [YaccProduction];
-
-abstract production yaccProductionList
-top::YaccProductionList ::= p::YaccProduction ps::YaccProductionList
-{
-  top.prods = cons(p, ps.prods);
-}
-
-abstract production yaccNilProductionList
-top::YaccProductionList ::=
-{
-  top.prods = nil();
-}
-
-nonterminal YaccProduction;
-
-abstract production yaccProduction
-top::YaccProduction ::=
-{
-}
-
-nonterminal YaccProductionAlternativeList with prodAlts;
-synthesized attribute prodAlts :: [YaccProductionAlternative];
-
-abstract production yaccProductionAlternativeList
-top::YaccProductionAlternativeList ::= pa::YaccProductionAlternative pas::YaccProductionAlternativeList
-{
-  top.prodAlts = cons(pa, pas.prodAlts);
-}
-
-abstract production yaccNilProductionAlternativeList
-top::YaccProductionAlternativeList ::=
-{
-  top.prodAlts = nil();
-}
-
-nonterminal YaccProductionAlternative;
-
-abstract production yaccProductionAlternative
-top::YaccProductionAlternative ::=
-{
-}
-
-nonterminal YaccSymbolOrActionList;
-
-abstract production yaccSymbolOrActionList
-top::YaccSymbolOrActionList ::=
-{
-}
-
-abstract production yaccNilSymbolOrActionList
-top::YaccSymbolOrActionList ::=
-{
-}
-
-nonterminal YaccSymbolOrAction;
-
-abstract production yaccSymbol
-top::YaccSymbolOrAction ::=
-{
-}
-
-abstract production yaccSemanticAction
-top::YaccSymbolOrAction ::= s::abs:Stmt
-{
-}
-
-abstract production yaccNilSemanticAction
-top::YaccSymbolOrAction ::=
-{
-}
-
---abstract production yaccDefinitionSection
---top::abs:Expr ::= l1::String
---{
-----  forwards to Expr(l1);
---}
-
---abstract production yaccDefinition
---top::abs:expr ::= e::expr
---{
---  forwards to expr(e);
---}
-
---abstract production myExpr
---top::abs:Expr ::= e::abs:expr
---{
---  forwards to expr(e);
---}
---
--- extensions/yacc/edu.umn.cs.melt.exts.ableC.yacc/concretesyntax/ConcreteSyntax.sv
+++ extensions/yacc/edu.umn.cs.melt.exts.ableC.yacc/concretesyntax/ConcreteSyntax.sv
@@ -1,9 +1,8 @@
 grammar edu:umn:cs:melt:exts:ableC:yacc:concretesyntax;
 
 imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
-
+imports edu:umn:cs:melt:ableC:abstractsyntax as ableCabs;
 imports edu:umn:cs:melt:exts:ableC:yacc:abstractsyntax as abs;
-
 import silver:langutil;
 
 marking terminal YaccGrammar_t '%%' lexer classes {Ckeyword};
@@ -12,89 +11,83 @@ marking terminal YaccStart_t '%start' lexer classes {Ckeyword};
 marking terminal YaccLeft_t '%left' lexer classes {Ckeyword};
 
 concrete production yaccGrammar_c
-top::cnc:Declaration_c ::= YaccDeclList_c YaccGrammar_t p::YaccProductionList_c YaccGrammar_t
+top::cnc:Stmt_c ::= YaccDeclList_c '%%' p::YaccProductionList_c '%%'
 {
   top.ast = abs:yaccGrammar(p.ast);
 }
 
 nonterminal YaccDeclList_c with ast<abs:YaccDeclList>, location;
-
 concrete productions top::YaccDeclList_c
 | ps::YaccDeclList_c p::YaccDecl_c
     {
-      top.ast = abs:yaccDeclList(p.ast, ps.ast);
+      top.ast = abs:yaccConsDecl(p.ast, ps.ast);
     }
 |
     {
-      top.ast = abs:yaccNilDeclList();
+      top.ast = abs:yaccNilDecl();
     }
 
 nonterminal YaccDecl_c with ast<abs:YaccDecl>, location;
-
 concrete productions top::YaccDecl_c
-| YaccToken_t cnc:Identifier_t
+| '%token' n::cnc:Identifier_t
   {
-    top.ast = abs:yaccDecl();
+    top.ast = abs:yaccTokenDecl(abs:fromId(n));
   }
-| YaccStart_t cnc:Identifier_t
+| '%start' n::cnc:Identifier_t
   {
-    top.ast = abs:yaccDecl();
+    top.ast = abs:yaccStartDecl(abs:fromId(n));
   }
-| YaccLeft_t YaccIdentifierList_c
+| '%left' ns::YaccIdentifierList_c
   {
-    top.ast = abs:yaccDecl();
+    top.ast = abs:yaccLeftDecl(ns.ast);
   }
 
 nonterminal YaccIdentifierList_c with ast<abs:YaccNameList>, location;
-
 concrete productions top::YaccIdentifierList_c
 | is::YaccIdentifierList_c i::cnc:Identifier_t
   {
-    top.ast = abs:yaccNameList(abs:fromId(i), is.ast);
+    top.ast = abs:yaccConsName(abs:fromId(i), is.ast);
   }
 | i::cnc:Identifier_t
   {
-    top.ast = abs:yaccNameList(abs:fromId(i), abs:yaccNilNameList());
+    top.ast = abs:yaccConsName(abs:fromId(i), abs:yaccNilName());
   }
 
 nonterminal YaccProductionList_c with ast<abs:YaccProductionList>, location;
-
 concrete productions top::YaccProductionList_c
 | ps::YaccProductionList_c p::YaccProduction_c
     {
-      top.ast = abs:yaccProductionList(p.ast, ps.ast);
+      top.ast = abs:yaccConsProduction(p.ast, ps.ast);
     }
 |
     {
-      top.ast = abs:yaccNilProductionList();
+      top.ast = abs:yaccNilProduction();
     }
 
 nonterminal YaccProduction_c with ast<abs:YaccProduction>, location;
-
 concrete production yaccProduction_c
-top::YaccProduction_c ::= cnc:Identifier_t ':' YaccProductionAlternativeList_c ';'
+top::YaccProduction_c ::= leftNt::cnc:Identifier_t ':' pas::YaccProductionAlternativeList_c ';'
 {
-  top.ast = abs:yaccProduction();
+  top.ast = abs:yaccProduction(abs:fromId(leftNt), pas.ast);
 }
 
 nonterminal YaccProductionAlternativeList_c with ast<abs:YaccProductionAlternativeList>, location;
-
 concrete productions top::YaccProductionAlternativeList_c
 | pas::YaccProductionAlternativeList_c '|' pa::YaccProductionAlternative_c
     {
-      top.ast = abs:yaccProductionAlternativeList(pa.ast, pas.ast);
+      top.ast = abs:yaccConsProductionAlternative(pa.ast, pas.ast);
     }
 | YaccProductionAlternative_c
     {
-      top.ast = abs:yaccNilProductionAlternativeList();
+      top.ast = abs:yaccNilProductionAlternative();
     }
 
 nonterminal YaccProductionAlternative_c with ast<abs:YaccProductionAlternative>, location;
 
 concrete production yaccProductionAlternative_c
-top::YaccProductionAlternative_c ::= YaccSymbolOrActionList_c
+top::YaccProductionAlternative_c ::= s::YaccSymbolOrActionList_c
 {
-  top.ast = abs:yaccProductionAlternative();
+  top.ast = abs:yaccProductionAlternative(s.ast);
 }
 
 nonterminal YaccSymbolOrActionList_c with ast<abs:YaccSymbolOrActionList>, location;
@@ -102,18 +95,18 @@ nonterminal YaccSymbolOrActionList_c with ast<abs:YaccSymbolOrActionList>, locat
 concrete productions top::YaccSymbolOrActionList_c
 | sas::YaccSymbolOrActionList_c sa::YaccSymbolOrAction_c
     {
-      top.ast = abs:yaccSymbolOrActionList();
+      top.ast = abs:yaccConsSymbolOrAction(sa.ast, sas.ast);
     }
 |
     {
-      top.ast = abs:yaccNilSymbolOrActionList();
+      top.ast = abs:yaccNilSymbolOrAction();
     }
 
 nonterminal YaccSymbolOrAction_c with ast<abs:YaccSymbolOrAction>, location;
 concrete productions top::YaccSymbolOrAction_c
-| cnc:Identifier_t
+| s::cnc:Identifier_t
     {
-      top.ast = abs:yaccSymbol();
+      top.ast = abs:yaccSymbol(abs:fromId(s));
     }
 | '{' sa::YaccSemanticAction_c '}'
     {
@@ -128,40 +121,6 @@ concrete productions top::YaccSemanticAction_c
   }
 |
   {
-    top.ast = abs:yaccNilSemanticAction();
+    top.ast = abs:yaccSemanticAction(ableCabs:nullStmt());
   }
 
-
-----marking terminal YaccDefinition_t '%';
---marking terminal YaccDefinition_t 'EXPR' lexer classes {Ckeyword};
---
-----terminal YaccToken 'union';
-----terminal YaccToken 'token';
-----terminal YaccStart 'start';
-----terminal YaccLeft  'left';
-----terminal YaccRight 'right';
-----terminal YaccType  'type';
---
-----concrete productions top::Expr_c
-------| YaccDefinition_t s::PrimaryExpr_c
-----| 'EXPR' s::Expr_c
-----    { top.ast = s.ast; }
---
---nonterminal YaccDefinition_c with ast<ast:Expr>;
---
---concrete production yaccDefinition_c
---top::YaccDefinition_c ::= YaccDefinition_t s::Expr_c
-----top::cnc:Stmt_c ::= YaccDefinition_t s::Stmt_c
---{
-----  top = yaccDefinition(e)
---  top.ast = s.ast
---}
-
---marking terminal MyExpr_t 'EXPR' lexer classes {Ckeyword};
---
---concrete production myExpr_c
---top::cnc:Expr_c ::= MyExpr_t e::cnc:Expr_c
---{
---  top = MyExpr(e);
---}
---
